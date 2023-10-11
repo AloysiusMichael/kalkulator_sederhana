4b251c1b96e762c6b6ce52fb4ce09318e2529764
AloysiusMichael <126872731+AloysiusMichael@users.noreply.github.com>
2023-10-11 21:37:41 +0700

    Initial commit

---
+++ bin/kalkulator.dart
@@ -0,0 +1,21 @@
+class kalkulator {
+  double kal1;
+  double kal2;
+  kalkulator(this.kal1, this.kal2);
+
+  double Tambah(kal1, kal2) {
+    return kal1 + kal2;
+  }
+
+  double Kurang(kal1, kal2) {
+    return kal1 - kal2;
+  }
+
+  double Kali(kal1, kal2) {
+    return kal1 * kal2;
+  }
+
+  double Bagi(kal1, kal2) {
+    return kal1 / kal2;
+  }
+}
