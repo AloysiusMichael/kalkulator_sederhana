1d2213eb7c4efb1dec1d573a32a02dfd27d9602e
Devin Marselio <devinmarselio@mhs.mdp.ac.id>
2023-10-12 21:28:11 +0700

    Penambahan perulangan pada class main kalkulator sederhana

--- bin/kalkulator_sederhana.dart
+++ bin/kalkulator_sederhana.dart
@@ -1,5 +1,31 @@
+import 'dart:io';
+import 'kalkulator.dart';
+
+class a extends kalkulator{
+  a(double kal1, double kal2) : super(kal1, kal2);
+}
+
+void main() {
+  var ulg = true;
+  while (ulg == true){
+    var ulg2 = true;
+    while (ulg2 == true){
+      //Pengecekan dan menampilkan error menggunakan try/catch
+      print("");
+      try{
+        stdout.write("Bilangan 1 = ");
+        double? kal1 = double.parse(stdin.readLineSync()!);
+        stdout.write("Bilangan 2 = ");
+        double? kal2 = double.parse(stdin.readLineSync()!);
+        
+        ulg2 = false;
+        print("");
+        print("1.Tambah" + " 2.Kurang" + " 3.Kali" + " 4.Bagi");
+        stdout.write("Pilih = ");
+        String? jawab3 = stdin.readLineSync();
 
 
-void main(List<String> arguments) {
-  
+      }
+    }
+  }
 }
